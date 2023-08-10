ba3c4034be5f54599abc0956f7b96ed5b7931972
Shammaa-Alsomaikhi <shammaa.a.s@gmail.com>
2023-08-10 20:20:59 +0300

    update project

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 export AUTH0_DOMAIN='shammaa-coffe-shop.us.auth0.com'
 export API_AUDIENCE='casting_agency'
 export ALGORITHMS=['RS256']
-export DATABASE_URL='postgres://lindachen@localhost:5432/casting_agency'
+export DATABASE_URL='postgres://shammaaas@localhost:5432/casting_agency'
 export EXCITED="true"
