f8e1684e25efee21e246e1ed8bd5726c95ad0458
Shammaa-Alsomaikhi <shammaa.a.s@gmail.com>
2023-08-10 20:38:01 +0300

    update url

--- models.py
+++ models.py
@@ -6,10 +6,11 @@ import json
 
 
 
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
-DB_USER = os.getenv('DB_USER', 'shammaaas')  
-DB_NAME = os.getenv('DB_NAME', 'casting_agency')
-DB_PATH = 'postgresql+psycopg2://{}@{}/{}'.format(DB_USER, DB_HOST, DB_NAME)
+# DB_HOST = os.getenv('DB_HOST', 'e:5432')  
+# DB_USER = os.getenv('DB_USER', 'shammaaas')  
+# DB_NAME = os.getenv('DB_NAME', 'casting_agency')
+# DB_PATH = 'postgresql+psycopg2://{}@{}/{}'.format(DB_USER, DB_HOST, DB_NAME)
+DB_PATH = os.environ['DATABASE_URL']
 
 
 
